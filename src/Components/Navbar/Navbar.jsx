27fe99f764f53186a2318147d302dc6b72eb1244
JerushanL <jerujerooshan995@gmail.com>
2024-08-07 22:52:41 -0400

    Your commit message

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import './Navbar.css'
 import logo from "../Assets/logo.png"
 import cart_icon from '../Assets/cart_icon.png'
 import dropdown_arrow from '../Assets/nav_dropdown.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
 
@@ -20,11 +22,16 @@ const Navbar = () => {
 
   return (
     <div className="navbar" >
-      <Link to={"/"} style={{ textDecoration: "none" }} ><div className="nav-logo">
-        <img src={logo} alt="" />
-        <p>Shoppers</p>
-      </div></Link>
-      <img className='nav-dropdown' src={dropdown_arrow} onClick={dropdown_toggle} />
+      {/* <img className='nav-dropdown' src={dropdown_arrow} onClick={dropdown_toggle} /> */}
+      <div className="nav-logo">
+        <FontAwesomeIcon icon={faBars} onClick={dropdown_toggle} className='nav-dropdown' />
+        <Link to={"/"} style={{ textDecoration: "none" }} >
+          <div className="nav-logo-2">
+            <img src={logo} alt="" />
+            <p>Shoppers</p>
+          </div></Link>
+      </div>
+
       <ul ref={menuRef} className="nav-menu">
         <li onClick={() => { setMenu("shop") }}><Link style={{ textDecoration: "none", color: "#272727" }} to={'/'}>Shop</Link> {menu === "shop" ? <hr /> : <></>} </li>
         <li onClick={() => { setMenu("men") }}><Link style={{ textDecoration: "none", color: "#272727" }} to={'/men'} >Men</Link> {menu === "men" ? <hr /> : <></>} </li>
